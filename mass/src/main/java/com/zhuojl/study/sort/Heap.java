8df7f4a48a20e5b5cc5047f6c4122bec1f11ac81
zhuojunliang <zhuojunliang_cd@keruyun.com>
2019-11-05 08:24:39 +0800

    remove no useful lines

--- mass/src/main/java/com/zhuojl/study/sort/Heap.java
+++ mass/src/main/java/com/zhuojl/study/sort/Heap.java
@@ -14,15 +14,6 @@ public class Heap {
 
     public static void test(Integer[] args) {
         heapFy(args);
-        int deleted;
-        int child;
-        for (int length = args.length; length > 0; ) {
-            deleted = args[--length];
-            args[length] = args[0];
-            // 找左右子节点最小的一个,留着下次做
-            int index = 0;
-            child = 2 * index + 1;
-        }
     }
 
     private static void heapFy(Integer[] args) {
